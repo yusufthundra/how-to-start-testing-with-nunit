5088f2ac050d6b287432fc95a1bafa81ddfc9970
Tomasz Wiśniewski <tomasz.wisniewski.gtms@gmail.com>
2018-03-16 13:23:18 +0100

    First release

---
+++ HowToStartTestingWithNUnit.Tests/MathUtilsTests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowToStartTestingWithNUnit.Tests
+{
+    [TestFixture]
+    public class MathUtilsTests
+    {
+        // First version
+        //[Test]
+        //public void CalculateFibonacciNumberShouldReturnCorrectValue()
+        //{
+        //    // Arrage
+        //    var numberIndex = 6;
+        //    var expected = 8;
+
+        //    // Act
+        //    var actual = MathUtils.CalculateFibonacciNumber(numberIndex);
+
+        //    // Assert
+        //    Assert.That(actual, Is.EqualTo(expected));
+        //}
+
+
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(6, 8)]
+        [TestCase(19, 4181)]
+        public void CalculateFibonacciNumberShouldReturnCorrectValue(int numberIndex, int expected)
+        {
+            // Arrage
+            // We only need parameters
+
+            // Act
+            var actual = MathUtils.CalculateFibonacciNumber(numberIndex);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CalculateFibonacciNumberShouldTrowArgumentExceptionForNegativeArgument()
+        {
+            // Arrage
+            var numberIndex = -1;
+
+            // Act
+            TestDelegate actual = () => MathUtils.CalculateFibonacciNumber(numberIndex);
+
+            // Assert
+            Assert.That(actual, Throws.ArgumentException);
+        }
+
+        //[Test]
+        //public void GetFibonacciSequenceShouldReturnCorrectSequence()
+        //{
+        //    // Arrage
+        //    var expected = new List<int> { 0, 1, 1, 2 };
+
+        //    // Act
+        //    var actual = MathUtils.GetFibonacciSequence(3);
+
+        //    // Assert
+        //    CollectionAssert.AreEqual(expected, actual);
+        //}
+
+        [TestCaseSource(nameof(_getFibonacciSequenceShouldReturnCorrectSequenceData))]
+        public void GetFibonacciSequenceShouldReturnCorrectSequence(int indexNumber, List<int> expected)
+        {
+            // Arrage
+
+            // Act
+            var actual = MathUtils.GetFibonacciSequence(indexNumber);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        static object[] _getFibonacciSequenceShouldReturnCorrectSequenceData =
+        {
+            new object[] { 0, new List<int> { 0 } },
+            new object[] { 1, new List<int> { 0, 1 } },
+            new object[] { 2, new List<int> { 0, 1, 1 } },
+            new object[] { 3, new List<int> { 0, 1, 1, 2 } }
+        };
+    }
+}
---
+++ HowToStartTestingWithNUnit/MathUtils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowToStartTestingWithNUnit
+{
+    public static class MathUtils
+    {
+        // First version
+        //public static int CalculateFibonacciNumber(int numberIndex)
+        //{
+        //    if (numberIndex == 1 || numberIndex == 2)
+        //    {
+        //        return 1;
+        //    }
+
+        //    return CalculateFibonacciNumber(numberIndex - 1) + CalculateFibonacciNumber(numberIndex - 2);
+        //}
+
+        // Second version
+        //public static int CalculateFibonacciNumber(int numberIndex)
+        //{
+        //    if (numberIndex == 0 || numberIndex == 1)
+        //    {
+        //        return numberIndex;
+        //    }
+            
+        //    return CalculateFibonacciNumber(numberIndex - 1) + CalculateFibonacciNumber(numberIndex - 2);
+        //}
+
+        public static int CalculateFibonacciNumber(int numberIndex)
+        {
+            if(numberIndex < 0)
+            {
+                throw new ArgumentException("Index cannot be negative.");
+            }
+
+            if (numberIndex == 0 || numberIndex == 1)
+            {
+                return numberIndex;
+            }
+
+            return CalculateFibonacciNumber(numberIndex - 1) + CalculateFibonacciNumber(numberIndex - 2);
+        }
+
+        public static List<int> GetFibonacciSequence(int maxNumberIndexInclusive)
+        {
+            List<int> result = new List<int>();
+
+            for(int i = 0; i <= maxNumberIndexInclusive; ++i)
+            {
+                result.Add(CalculateFibonacciNumber(i));
+            }
+
+            return result;
+        }
+    }
+}
